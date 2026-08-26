4ae1904815f83f776a684be7b0d49616e9cd1527
Egrum12 <130387535+sdsdsdsrj4rjj@users.noreply.github.com>
2026-08-26 09:55:19 -0500

    UUUpppdddaaattteee RRReeennndddeeerrrPPPlllaaayyyeeerrr...jjjaaavvvaaa

--- src/main/java/net/minecraft/client/renderer/entity/RenderPlayer.java
+++ src/main/java/net/minecraft/client/renderer/entity/RenderPlayer.java
@@ -129,17 +129,12 @@ public class RenderPlayer extends RendererLivingEntity<AbstractClientPlayer> {
 
 	}
 
-	// fixed texture used by every player, replacing the downloaded 64x64
-	// per-player skin -- matches the 16x16 UV layout baked into ModelPlayer
-	private static final ResourceLocation CUSTOM_PLAYER_TEXTURE = new ResourceLocation(
-			"textures/entity/custom_player.png");
-
 	/**+
 	 * Returns the location of an entity's texture. Doesn't seem to
 	 * be called unless you call Render.bindEntityTexture.
 	 */
 	protected ResourceLocation getEntityTexture(AbstractClientPlayer abstractclientplayer) {
-		return CUSTOM_PLAYER_TEXTURE;
+		return abstractclientplayer.getLocationSkin();
 	}
 
 	public void transformHeldFull3DItemLayer() {
